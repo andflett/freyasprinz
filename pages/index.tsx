3a3b6932177522bcaadc4cb4f39c259913d2f8bf
Andrew Flett <andrflett@gmail.com>
2023-05-18 17:57:32 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { Shh } from "~/Images/Shh";
 import { Layout } from "~/Layout";
 
 const Home: NextPage = () => {
@@ -20,32 +19,7 @@ const Home: NextPage = () => {
         minW={{ base: "full", md: "5xl" }}
       >
         <Box bg="white" padding={20} boxShadow="md">
-          <Stack spacing="10">
-            <Heading size={{ base: "3xl", md: "4xl" }} color="orange">
-              Freya Sprinz’s website
-            </Heading>
-            <Flex
-              justify={"space-between"}
-              direction={{ base: "column", md: "row" }}
-            >
-              <Stack spacing="3">
-                <Heading fontFamily="Roboto Mono" size="xl">
-                  Song 1 - Secret
-                </Heading>
-                <Text>Oh, you told her our secret.</Text>
-              </Stack>
-              <Shh />
-            </Flex>
-            <Stack spacing="3">
-              <Heading fontFamily="Roboto Mono" size="xl">
-                How to be kind
-              </Heading>
-              <UnorderedList pl="5">
-                <ListItem>Ask if people are ok when they are sad</ListItem>
-                <ListItem>Help other people</ListItem>
-              </UnorderedList>
-            </Stack>
-          </Stack>
+         
         </Box>
       </Container>
     </Layout>
